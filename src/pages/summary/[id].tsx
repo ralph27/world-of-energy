0730e633009b421aa31d6fb19349ba926025bc45
ralph27 <maaloulyralph@gmail.com>
2023-05-10 15:39:54 +0300

    fixing summary responsivness

--- src/pages/summary/[id].tsx
+++ src/pages/summary/[id].tsx
@@ -32,24 +32,24 @@ export default function Page({ menuOpen }: { menuOpen: boolean }) {
   return (
     <div className={`flex ${kanit.variable} ${roboto.variable} pb-10 bg-base200`}>
       <LeftTab menuOpen={menuOpen} />
-      <div className="mx-auto my-0 w-full pt-5 lg:w-11/12 ">
-        <div className='px-10'>
+      <div className="mx-auto my-0 w-full pt-5 lg:w-3/4 max-w-7xl">
+        <div className='px-3 sm:px-10'>
           <div className="font-kanit">
             <Category
               category={summary.data?.category.name || ''}
               color={summary.data?.category.background || ''}
             />
-            <h1 className="pt-4 text-4xl text-white sm:text-5xl md:text-6xl lg:text-7xl">
+            <h1 className="pt-4 text-3xl text-white sm:text-5xl lg:text-6xl">
               {summary.data?.title}
             </h1>
           </div>
-          <div className="relative my-6 px-10 h-64 w-full sm:h-80 md:h-96 lg:mx-auto lg:my-10 lg:h-100">
+          <div className="relative my-6 sm:px-10 h-64 w-full sm:h-80 md:h-96 lg:mx-auto lg:my-10 lg:h-100">
             <Image src={summary.data?.image || ''} fill alt="picture" className='rounded-lg' />
           </div>
         </div>
         <div className="mx-auto my-0 max-w-3xl items-center pl-3 pr-3 leading-7 text-[#d1d5db] sm:px-6">
           <Subscribe />
-          <ReactMarkdown className="mt-5 font-kanit text-base sm:text-xl lg:text-2xl">
+          <ReactMarkdown className="mt-5 font-kanit text-xl sm:text-xl lg:text-2xl">
             {summary.data?.content || ''}
           </ReactMarkdown>
         </div>
